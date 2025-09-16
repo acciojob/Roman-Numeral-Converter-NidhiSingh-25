b6bad9d17fb9a22675a4358180d395bf4577ef24
NidhiSingh-25 <nidhi630694@gmail.com>
2025-09-16 17:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function convertToRoman(num) {
 
   //your code here
 	let result="";
-	const val=[['M',1000],['CM',900],['D',500],['CD',400],['C',100],['XC',90],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]];
+	const values=[['M',1000],['CM',900],['D',500],['CD',400],['C',100],['XC',90],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]];
 	for(let [roman,val] of values){
 		while(num>=val){
 			result+=roman;
